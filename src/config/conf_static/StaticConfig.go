47a8846681933db3c67b09385d9b35562256df88
Mooningway <Puddingway@hotmail.com>
2022-06-02 15:17:05 +0800

    update configuration and add bookmark tag

--- src/config/conf_static/StaticConfig.go
+++ src/config/conf_static/StaticConfig.go
@@ -2,14 +2,18 @@ package conf_static
 
 // Static resource configuration
 import (
+	"my-project/src/controller/c_bookmark_tag"
 	"my-project/src/controller/c_exchange_rate"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Config(engine *gin.Engine) {
+	engine.Delims(`{{{`, `}}}`)
+
 	engine.LoadHTMLGlob(`template/**/*`)
 	engine.Static(`static`, `static`)
 
 	engine.GET(`/exrate`, c_exchange_rate.Index)
+	engine.GET(`/bookmark/tag`, c_bookmark_tag.Index)
 }
